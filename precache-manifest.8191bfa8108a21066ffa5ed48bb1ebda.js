b415067ff755a2f96a6f00b803d102c17c0fcadc
Mark Kenneth G. Esguerra <esguerrakenneth@gmail.com>
2020-04-11 17:29:02 +0800

    Updates

---
+++ precache-manifest.8191bfa8108a21066ffa5ed48bb1ebda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975385cf95c1418d5b381c712f774472",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "df42ec73a32a87661eb3",
+    "url": "/covid-19-tracker/static/css/main.9e1760a1.chunk.css"
+  },
+  {
+    "revision": "6fb8544c6cdbad835e8d",
+    "url": "/covid-19-tracker/static/js/2.89e0a778.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-tracker/static/js/2.89e0a778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df42ec73a32a87661eb3",
+    "url": "/covid-19-tracker/static/js/main.2f186650.chunk.js"
+  },
+  {
+    "revision": "3d1c3a839b5135420af2",
+    "url": "/covid-19-tracker/static/js/runtime-main.94eb6b52.js"
+  },
+  {
+    "revision": "29f7b0b2722abf2acbb0e5617f48ce20",
+    "url": "/covid-19-tracker/static/media/coronavirus.29f7b0b2.png"
+  }
+]);
