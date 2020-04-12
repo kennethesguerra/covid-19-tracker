d1517fd73e7504a27c4b6eb431f0df434f1932b5
Mark Kenneth G. Esguerra <esguerrakenneth@gmail.com>
2020-04-12 21:00:06 +0800

    Fixing redirection for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Worldwide from './components/Worldwide';
 import GlobalDailyStatus from './components/GlobalDailyStatus';
@@ -7,11 +7,11 @@ import GlobalDailyStatus from './components/GlobalDailyStatus';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter basename='/'>
         <div className="App">
           <div className="row blue darken-4 header">
             <div className="col s12 m9">
-            <Link to='/covid-19-stat/'><h5>COVID-19 Pandemic Status</h5></Link>
+            <Link to='/'><h5>COVID-19 Pandemic Status</h5></Link>
             </div>
             <div className="col s12 m3">
               <a href="https://github.com/kennethesguerra/covid-19-stat"><i className="fab fa-github fa-3x right"></i></a>
@@ -19,15 +19,15 @@ class App extends Component {
           </div>
           <div className="home-page container">
             <Switch>
-              <Route exact path='/covid-19-stat/' component={Home}></Route>
-              <Route exact path='/covid-19-stat/worldwide' component={Worldwide}></Route>
-              <Route exact path='/covid-19-stat/daily' component={GlobalDailyStatus}></Route>
+              <Route exact path='/' component={Home}></Route>
+              <Route exact path='/worldwide' component={Worldwide}></Route>
+              <Route exact path='/daily' component={GlobalDailyStatus}></Route>
             </Switch>
           </div>
           <p className="center grey-text">Visualization by<a href='http://github.com/kennethesguerra'> @kennethesguerra</a><br />
           Data access from John Hopkins University CSSE via <a href="http://github.com/mathdroid"> @mathdroid</a>'s Covid-19 API</p>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
   
